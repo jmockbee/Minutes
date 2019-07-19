80fe5efa699a85d86fcbb9e8c2d052ffa0a63f6b
Jon Mockbee <mockbee1000@gmail.com>
2019-07-19 14:00:31 -0400

    put 0 in front of numbers make them look  better

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,53 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(65, 45));
         System.out.println(getDurationString(3945));
     }
 
-    private static String getDurationString(int minutes, int seconds){
-       if ((minutes <0)|| (seconds <0) || (seconds>59))
-        {
+    private static String getDurationString(int minutes, int seconds) {
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
             return "invalid value";
         }
 
-        int hours = minutes/60;
-        int remainingMinutes = minutes%60;
-        return hours +"h " +remainingMinutes+  "m " + seconds+ "s ";
+
+
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+
+        String hoursString = hours +"h ";
+        if (hours<10){
+         hoursString ="0"+hoursString;
+        }
+
+        String minutesString = remainingMinutes +"m ";
+        if (minutes<10){
+            minutesString  ="0"+ minutesString;
+        }
+
+        String secondsString = seconds +"s ";
+        if (minutes<10){
+            secondsString  ="0"+ secondsString;
+        }
+
+
+
+        return hoursString + " " + minutesString +" " + secondsString + " ";
     }
-    private static String getDurationString(int seconds){
-        if (seconds<0) {
+
+    private static String getDurationString(int seconds) {
+        if (seconds < 0) {
             return "invalid";
         }
-     int minutes = seconds/60;
-        int remainingSeconds = seconds% 60;
-        return getDurationString(minutes,remainingSeconds);
 
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
 
-
+        return getDurationString(minutes, remainingSeconds);
     }
 }
+
+
+
+
